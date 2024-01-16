4bb47a86099acf528a92c7d7f7c9df8a473c1878
ss-thapa <145964403+ss-thapa@users.noreply.github.com>
2024-01-16 17:09:34 +0545

    Update webscrapping.py

--- webscrapping.py
+++ webscrapping.py
@@ -1,33 +1,63 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
-page_num = 0
-rows=[]
-for i in range(1, 7):
-    page_num = page_num + 1
-    url = 'http://www.scrapethissite.com/pages/forms/?page_num=' + str(page_num) + '&per_page=100'
-    print(page_num)
-    page = requests.get(url)
 
-    soup = BeautifulSoup(page.text, 'html.parser')
 
-    if i == 1:
-        title = soup.find_all('th')
-        title_names = [data.get_text(strip=True) for data in title]
+ 
+# page_num = 0
+# rows=[]
+# for i in range(1, 7):
+#     page_num = page_num + 1
+#     url = 'http://www.scrapethissite.com/pages/forms/?page_num=' + str(page_num) + '&per_page=100'
+#     page = requests.get(url)
 
-        df = pd.DataFrame(columns=title_names)
+#     soup = BeautifulSoup(page.text, 'html.parser')
 
-    row_data = soup.find_all('td')
+#     if i == 1:
+#         title = soup.find_all('th')
+#         title_names = [data.get_text(strip=True) for data in title]
 
-    individeual_row_data = [data.get_text(strip=True) for data in row_data]
+#         df = pd.DataFrame(columns=title_names)
 
-    chunk_size = len(title_names)
-    for i in range(0, len(individeual_row_data), chunk_size):
-        row_data = individeual_row_data[i:i + chunk_size]
-        rows.append(row_data)
+#     row_data = soup.find_all('td')
+
+#     individeual_row_data = [data.get_text(strip=True) for data in row_data]
+
+#     chunk_size = len(title_names)
+#     for i in range(0, len(individeual_row_data), chunk_size):
+#         row_data = individeual_row_data[i:i + chunk_size]
+#         rows.append(row_data)
 
     
-df = pd.DataFrame(rows, columns = title_names)
+# df = pd.DataFrame(rows, columns = title_names)
+
+
+
+
+# url = 'https://www.forbes.com/real-time-billionaires/#4b42941b3d78'
+
+# page = requests.get(url)
+
+# soup = BeautifulSoup(page.text, 'html.parser')
+
+# print(soup.find('div',attrs = {'class':"scrolly-table"}))
+
+
+
+
+# titles = soup.find_all()
+
+# print(titles)
+
+# title_name = [data.get_text for data in titles]
+
+
+
+
+
+
+
+
 
 
 
